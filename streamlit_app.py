efc14d8041ee8d228a5b93c71ee770967ff0cd32
umeshr123 <59060772+umeshr123@users.noreply.github.com>
2025-01-25 23:01:05 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,11 @@ st.write(
 #session = get_active_session()
 #st.write(session)
 my_dataframe = session.table("smoothies.public.fruit_options").select( col("FRUIT_NAME"), col("SEARCH_ON"))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df=my_dataframe.to_pandas()
+st.datafame(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
